07bb751bb1f96a928acfe6829b4c452ad6363fca
angjelinhila <ahila@utexas.edu>
2025-04-29 20:18:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -177,4 +177,5 @@ def update_dashboard(authors_filter, topics_filter, keyword):
 
     return pub_fig, topic_fig, topic_ts, citation_ts, table_data, tooltips, author_stats.to_dict("records"), coauthor_fig, bipartite_fig
 
-app.run(mode="external")
+app.run(host="0.0.0.0", port=8080)
+
